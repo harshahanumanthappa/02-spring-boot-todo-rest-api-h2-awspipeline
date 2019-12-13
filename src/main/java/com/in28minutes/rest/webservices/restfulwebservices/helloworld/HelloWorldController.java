e622edf7904f6041c883813b3fa0e2acb909dffe
harshahanumanthappa <58506367+harshahanumanthappa@users.noreply.github.com>
2019-12-13 15:00:51 +0530

    Update HelloWorldController.java

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -10,12 +10,12 @@ public class HelloWorldController {
 
 	@GetMapping(path = "/hello-world")
 	public String helloWorld() {
-		return "Hello World";
+		return "Hello Harsha!!! how are you?";
 	}
 
 	@GetMapping(path = "/hello-world-bean")
 	public HelloWorldBean helloWorldBean() {
-		return new HelloWorldBean("Hello World");
+		return new HelloWorldBean("Hello World!!!! I am Harsha");
 	}
 	
 	///hello-world/path-variable/in28minutes
